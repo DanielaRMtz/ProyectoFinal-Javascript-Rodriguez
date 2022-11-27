4963028f0796a82beb77baa17be9880de1e23346
DanielaRMtz <esdanyrdzmtz@hotmail.com>
2022-11-26 22:57:47 -0600

    Tercera Entrega

---
+++ src/stock.js
@@ -0,0 +1,70 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "PRY-1",
+        tipo: "playera",
+        desc: "Playera Caballero",
+        precio: 150,
+        talla: "L",
+        img: './public/images/playera.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "PRY-2",
+        tipo: "falda",
+        desc: "Falda Dama",
+        precio: 180,
+        talla: "L",
+        img: './public/images/falda.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "PRY-3",
+        tipo: "vestido",
+        desc: "Vestido Dama",
+        precio: 300,
+        talla: "L",
+        img: './public/images/vestido.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "PRY-4",
+        tipo: "jeans",
+        desc: "Jeans Dama",
+        precio: 350,
+        talla: "11",
+        img: './public/images/jeans.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "PRY-5",
+        tipo: "tenis",
+        desc: "Tenis Dama",
+        precio: 180,
+        talla: "25.5 cm",
+        img: './public/images/tenis.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "PRY-6",
+        tipo: "zapatillas",
+        desc: "Zapatillas de Tacón Dama",
+        precio: 180,
+        talla: "24.5 cm",
+        img: './public/images/zapatillas.jpg',
+        cantidad:1
+    }
+]
+
+export { productos };
